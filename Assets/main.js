b220360c303ad7bd9c04e2645ab4ccf8a885c09c
kangajie <roifanmarzuki@gmail.com>
2025-06-30 14:29:49 +0700

    update main.js

--- Assets/main.js
+++ Assets/main.js
@@ -3,7 +3,13 @@ document.addEventListener('contextmenu', function (e) {
     e.preventDefault();
 });
 
+// Prevent right click (mousedown and mouseup)
 document.addEventListener('mousedown', function (e) {
+    if (e.button === 2) { // Right click
+        e.preventDefault();
+    }
+});
+document.addEventListener('mouseup', function (e) {
     if (e.button === 2) { // Right click
         e.preventDefault();
     }
@@ -11,11 +17,11 @@ document.addEventListener('mousedown', function (e) {
 
 document.onkeydown = function (e) {
     // F12
-    if (e.keyCode == 123) return false;
+    if (e.key === 'F12') return false;
     // Ctrl+Shift+I/J/C
-    if (e.ctrlKey && e.shiftKey && (e.keyCode == 73 || e.keyCode == 74 || e.keyCode == 67)) return false;
+    if (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J' || e.key === 'C')) return false;
     // Ctrl+U
-    if (e.ctrlKey && e.keyCode == 85) return false;
+    if (e.ctrlKey && (e.key === 'u' || e.key === 'U')) return false;
     // Ctrl+S
-    if (e.ctrlKey && e.keyCode == 83) return false;
+    if (e.ctrlKey && (e.key === 's' || e.key === 'S')) return false;
 };
